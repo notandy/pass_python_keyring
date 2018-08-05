0b49c548db332b6ffe14b7122a6e29bc3c94209f
Theo Chatzimichos <tampakrap@gmail.com>
2018-08-05 13:51:02 +0200

    remove whitespace

--- pass.py
+++ pass.py
@@ -11,7 +11,7 @@ from subprocess import Popen, PIPE
 class Keyring(KeyringBackend):
     """Pass Keyring"""
 
-    def supported(self): 
+    def supported(self):
         return 0
 
     def get_password(self, service, username):
@@ -32,6 +32,6 @@ class Keyring(KeyringBackend):
 
     def delete_password(self, service, username):
         proc = Popen(['pass', 'rm', '--force', '/'.join([service,username])])
-        proc.wait() 
+        proc.wait()
         if(proc.returncode != 0):
             raise PasswordDeleteError("Password not found")
